0f1707b5c02859c0f0580ecaadc3515ae56aeca0
Arosh Athukorala <aroshmathukorala@gmail.com>
2026-05-26 17:01:50 +0530

    added toggle planned leave toggle button functionality

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import EmailForm from "./EmailForm";
 import EmailOutput from "./EmailOutput";
 
 const Content = () => {
+  const [plannedLeave, setPlannedLeave] = useState("Planned");
+
+  const handleLeaveButtonToggle = (value: string) => {
+    setPlannedLeave(value);
+  };
+
   return (
     <div className="flex flex-1 gap-10 w-7xl mx-auto pb-5">
-      <EmailForm />
+      <EmailForm selectedValue={plannedLeave} onChange={handleLeaveButtonToggle}/>
       <EmailOutput />
     </div>
   );
--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,7 +1,13 @@
 import { FaPlus } from "react-icons/fa";
 
-const EmailForm = () => {
-  
+type EmailFormProps = {
+  selectedValue: string;
+  onChange: (value: string) => void;
+};
+
+const toggleButtons = ["Planned", "Unplanned"];
+
+const EmailForm = ({ selectedValue, onChange }: EmailFormProps) => {
   return (
     <div className="flex-1 bg-emerald-100 dark:bg-slate-900 p-3.5 rounded-lg shadow-2xl shadow-slate-400 dark:shadow-emerald-900">
       <div className="flex justify-between items-center">
@@ -10,21 +16,20 @@ const EmailForm = () => {
           ADD LEAVE ENTRY
         </p>
         <div
-          className="inline-flex rounded-md shadow-xs -space-x-px bg-emerald-50 dark:bg-slate-800"
+          className="p-1 inline-flex rounded-md shadow-xs -space-x-px bg-emerald-50 dark:bg-slate-800"
           role="group"
         >
-          <button
-            type="button"
-            className="px-3 py-2 cursor-pointer"
-          >
-            Planned
-          </button>
-          <button
-            type="button"
-            className="px-3 py-2 cursor-pointer"
-          >
-            Unplanned
-          </button>
+          {toggleButtons.map((button) => (
+            <button
+              key={button}
+              type="button"
+              value={selectedValue}
+              className={`px-2 py-1 rounded-md cursor-pointer ${ button === selectedValue ? "bg-emerald-200 dark:bg-slate-900" : ""}`}
+              onClick={() => onChange(button)}
+            >
+              {button}
+            </button>
+          ))}
         </div>
       </div>
     </div>
